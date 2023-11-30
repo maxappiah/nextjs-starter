3d63bcaddcd2f0f515bad01f55e685406d7654f1
Max Appiah <mappiah@binaryvision.com>
2023-11-30 23:53:02 +0000

    convert and add new scss modules

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import styles from "./footer.module.scss";
 
 export default function Footer() {
   return (
     <footer>
+      <div className={styles.footer}>
         <p>© 2023</p>
+        </div>
     </footer>
   );
 }
--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,16 +1,19 @@
 import Link from 'next/link';
+import styles from './header.module.scss';
 
 export default function Header() {
   
     return (
 
     <header>
-      <nav>
+      <nav> 
+      <div className={styles.nav}>
         <ul>
           <li><Link href="/">Home</Link></li>
           <li><Link href="/about">About</Link></li>
           <li><Link href="/contact">Contact</Link></li>
         </ul>
+        </div>
       </nav>
     </header>
   );
